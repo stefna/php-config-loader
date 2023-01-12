9891821647ca835e0e2777434cdedad3fedc716f
Andreas Sundqvist <andreas@stefna.is>
2023-01-12 11:36:13 +0100

    [config] Change namespace from moya -> stefna

--- src/FileConfig.php
+++ src/FileConfig.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types=1);
 
-namespace Moya\Config;
+namespace Stefna\Config;
 
 final class FileConfig implements Config
 {
--- src/GetConfigTrait.php
+++ src/GetConfigTrait.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types=1);
 
-namespace Moya\Config;
+namespace Stefna\Config;
 
 use Stefna\Collection\ScalarMapTrait;
 
